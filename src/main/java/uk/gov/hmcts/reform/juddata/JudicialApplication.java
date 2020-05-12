5cfecc1942ead759161bff7804cbde169809577f
abhijit <Abhijit.diwan@hmcts.net>
2020-05-12 19:26:54 +0100

    refactor: boot application updated

--- src/main/java/uk/gov/hmcts/reform/juddata/JudicialApplication.java
+++ src/main/java/uk/gov/hmcts/reform/juddata/JudicialApplication.java
@@ -6,7 +6,8 @@ import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,7 +16,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(scanBasePackages = "uk.gov.hmcts.reform.juddata")
 @EnableScheduling
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
-public class JudicialApplication implements CommandLineRunner {
+public class JudicialApplication implements ApplicationRunner {
 
     @Autowired
     JobLauncher jobLauncher;
@@ -27,13 +28,13 @@ public class JudicialApplication implements CommandLineRunner {
     String jobName;
     
     public static void main(final String[] args) {
-        ApplicationContext context = SpringApplication.run(JudicialApplication.class, args);
+        ApplicationContext context = SpringApplication.run(JudicialApplication.class);
         SpringApplication.exit(context);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
         JobParameters params = new JobParametersBuilder()
                 .addString(jobName, String.valueOf(System.currentTimeMillis()))
                 .toJobParameters();
