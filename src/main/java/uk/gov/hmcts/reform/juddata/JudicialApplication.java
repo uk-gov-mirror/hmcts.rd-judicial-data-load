46d278a1f7881526376b32dbcb5290deb47fc132
abhijit <Abhijit.diwan@hmcts.net>
2020-05-14 20:00:42 +0100

    refactor: boot application updated

--- src/main/java/uk/gov/hmcts/reform/juddata/JudicialApplication.java
+++ src/main/java/uk/gov/hmcts/reform/juddata/JudicialApplication.java
@@ -2,23 +2,17 @@ package uk.gov.hmcts.reform.juddata;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "uk.gov.hmcts.reform.juddata")
-@EnableScheduling
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 @Slf4j
-public class JudicialApplication implements ApplicationRunner {
+public class JudicialApplication {
 
     @Autowired
     JobLauncher jobLauncher;
@@ -34,14 +28,5 @@ public class JudicialApplication implements ApplicationRunner {
         int exitCode = SpringApplication.exit(context);
         log.info("Judicial Application exiting with exit code " + exitCode);
         System.exit(exitCode);
-    }
-
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        JobParameters params = new JobParametersBuilder()
-                .addString(jobName, String.valueOf(System.currentTimeMillis()))
-                .toJobParameters();
-        jobLauncher.run(job, params);
     }
 }
