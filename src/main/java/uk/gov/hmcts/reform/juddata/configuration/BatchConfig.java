1604293bf95ffd040774775e018c057bee78008f
abhijit <Abhijit.diwan@hmcts.net>
2020-05-28 11:54:59 +0100

    1404: updated review comments

--- src/main/java/uk/gov/hmcts/reform/juddata/configuration/BatchConfig.java
+++ src/main/java/uk/gov/hmcts/reform/juddata/configuration/BatchConfig.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.juddata.configuration;
 
 import static net.logstash.logback.encoder.org.apache.commons.lang3.BooleanUtils.negate;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -12,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import uk.gov.hmcts.reform.juddata.camel.service.AuditProcessingService;
 import uk.gov.hmcts.reform.juddata.camel.task.IdempotentTask;
 import uk.gov.hmcts.reform.juddata.camel.task.LeafRouteTask;
@@ -20,6 +20,7 @@ import uk.gov.hmcts.reform.juddata.camel.task.ParentRouteTask;
 
 @Configuration
 @EnableBatchProcessing
+@Slf4j
 public class BatchConfig {
 
     @Autowired
@@ -72,10 +73,9 @@ public class BatchConfig {
     }
 
     @Bean
-    @Lazy
     public Job runRoutesJob() {
-
         if (negate(schedulerAuditProcessingService.isAuditingCompleted())) {
+            log.info("::running non idempotent job:");
             return jobs.get(jobName)
                     .incrementer(new RunIdIncrementer())
                     .start(stepLeafRoute())
@@ -83,6 +83,7 @@ public class BatchConfig {
                     .build();
         }
 
+        log.info("::running idempotent job::");
         return jobs.get(jobName).start(stepIdempotent()).build();
     }
 }
