8f75954559077df3a056f16b7cac89084d92b35d
nayeemshaik-hmcts <95764500+nayeemshaik-hmcts@users.noreply.github.com>
2022-06-16 10:32:44 +0100

    Mrd fallback latest (#445)
    * MRD changes reverted.(RDCC-4805/RDCC-4807/RDCC-4808/RDCC-4809/RDCC-4811)
    
    * MRD changes reverted.(RDCC-4805/RDCC-4807/RDCC-4808/RDCC-4809/RDCC-4811)
    
    * RDCC-4867-updated the judicial_location_mapping table data.
    
    * RDCC-4867-updated the judicial_location_mapping table data.
    
    * RDCC-4867-updated the judicial_location_mapping table data.
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/reform/juddata/camel/mapper/JudicialOfficeAuthorisationRowMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/juddata/camel/mapper/JudicialOfficeAuthorisationRowMapperTest.java
@@ -3,13 +3,13 @@ package uk.gov.hmcts.reform.juddata.camel.mapper;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.juddata.camel.binder.JudicialOfficeAuthorisation;
 
+import java.sql.Timestamp;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static uk.gov.hmcts.reform.juddata.camel.helper.JrdTestSupport.createJudicialOfficeAuthorisation;
-import static uk.gov.hmcts.reform.juddata.camel.util.CommonUtils.getDateTimeStamp;
 
 class JudicialOfficeAuthorisationRowMapperTest {
 
@@ -20,25 +20,19 @@ class JudicialOfficeAuthorisationRowMapperTest {
     void should_return_JudicialOfficeAuthorizationtRowMapper_response() {
 
         JudicialOfficeAuthorisation judicialOfficeAuthorisation =
-            createJudicialOfficeAuthorisation("28-04-2022 00:00:00");
+            createJudicialOfficeAuthorisation("2017-10-01 00:00:00.000");
 
         Map<String, Object> authMap = judicialOfficeAuthorisationRowMapper.getMap(judicialOfficeAuthorisation);
 
         assertNotNull(authMap.get("judicial_office_auth_id"));
         assertEquals("1", authMap.get("per_id"));
         assertEquals("jurisdiction", authMap.get("jurisdiction"));
-        assertEquals(getDateTimeStamp(judicialOfficeAuthorisation.getStartDate()), authMap.get("start_date"));
-        assertEquals(getDateTimeStamp(judicialOfficeAuthorisation.getEndDate()), authMap.get("end_date"));
+        assertEquals(Timestamp.valueOf((judicialOfficeAuthorisation.getStartDate())), authMap.get("start_date"));
+        assertEquals(Timestamp.valueOf((judicialOfficeAuthorisation.getEndDate())), authMap.get("end_date"));
         assertEquals(Long.valueOf("12345"), authMap.get("ticket_id"));
         assertEquals("lowerLevel", authMap.get("lower_level"));
         assertEquals("111", authMap.get("personal_code"));
         assertEquals("779321b3-3170-44a0-bc7d-b4decc2aea10", authMap.get("object_id"));
-        assertEquals(getDateTimeStamp("28-04-2022 00:00:00"),authMap
-            .get("mrd_created_time"));
-        assertEquals(getDateTimeStamp("28-05-2022 00:00:00"),authMap
-            .get("mrd_updated_time"));
-        assertEquals(getDateTimeStamp("28-06-2022 00:00:00"),authMap
-            .get("mrd_deleted_time"));
     }
 
     @Test
