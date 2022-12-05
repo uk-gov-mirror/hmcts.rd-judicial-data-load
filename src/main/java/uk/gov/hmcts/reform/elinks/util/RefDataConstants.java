a57e3c28e4676ca97db0be2827ca1ede716754c2
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2022-12-05 09:57:51 +0000

    error codes for elinks

--- src/main/java/uk/gov/hmcts/reform/elinks/util/RefDataConstants.java
+++ src/main/java/uk/gov/hmcts/reform/elinks/util/RefDataConstants.java
@@ -9,14 +9,14 @@ public class RefDataConstants {
     public static final String AUTHORIZATION = "Authorization";
     public static final String CONTENT_TYPE = "Content-Type";
 
-    public static final String BAD_REQUEST = "Bad Request";
-    public static final String FORBIDDEN_ERROR = "Forbidden Error: Access denied for invalid permissions";
+    public static final String BAD_REQUEST = "Syntax error or Bad Request";
+    public static final String FORBIDDEN_ERROR = "Your source IP address is not whitelisted";
     public static final String UNAUTHORIZED_ERROR =
-            "Unauthorized Error : The requested resource is restricted and requires authentication";
-    public static final String NO_DATA_FOUND = "The eLinks data could not be found";
+            "A valid access token hasn't been provided in the right form";
+    public static final String NO_DATA_FOUND = "The given attribute name does not exist in the reference data";
     public static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
 
-    public static final String TOO_MANY_REQUESTS = "Too Many Requests";
+    public static final String TOO_MANY_REQUESTS = "You have exceeded the request limit (20 requests in 20 seconds)";
 
 
 }
