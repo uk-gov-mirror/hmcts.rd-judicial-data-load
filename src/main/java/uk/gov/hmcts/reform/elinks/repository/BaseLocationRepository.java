ede7b40d763910792678663628d8b47078cedfd4
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-12-07 14:25:13 +0000

    RDCC-5936:Changes made to add unit test cases

--- src/main/java/uk/gov/hmcts/reform/elinks/repository/BaseLocationRepository.java
+++ src/main/java/uk/gov/hmcts/reform/elinks/repository/BaseLocationRepository.java
@@ -5,7 +5,6 @@ import uk.gov.hmcts.reform.elinks.domain.BaseLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-import uk.gov.hmcts.reform.elinks.domain.BaseLocation;
 @SuppressWarnings("all")
 public interface BaseLocationRepository extends JpaRepository<BaseLocation, String> {
 
