8ac8a7b900de358747517e4ad8bf5adf662bbfdd
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2022-12-07 15:56:05 +0000

    RESPONSE Changes

---
+++ src/main/java/uk/gov/hmcts/reform/elinks/controller/response/BaseLocationResponse.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.reform.elinks.controller.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BaseLocationResponse {
+
+    private String baseLocationId;
+
+    private String courtType;
+
+    private String courtName;
+
+    private String circuit;
+
+    private String area_of_expertise;
+
+
+}
