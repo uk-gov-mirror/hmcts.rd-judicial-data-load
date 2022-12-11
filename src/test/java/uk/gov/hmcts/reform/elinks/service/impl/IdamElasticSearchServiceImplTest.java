d6971c61e832c4e275dcddc1f3563b8b5b044223
nayeemshaik-hmcts <nayeem.shaik@hmcts.net>
2022-12-11 09:46:45 +0000

    fixed the junit test cases

--- src/test/java/uk/gov/hmcts/reform/elinks/service/impl/IdamElasticSearchServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/elinks/service/impl/IdamElasticSearchServiceImplTest.java
@@ -34,7 +34,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
