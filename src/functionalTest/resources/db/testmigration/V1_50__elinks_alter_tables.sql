251c7f1761604d9b24e5cb805de8a5577c2118aa
shivajihmcts <89258035+shivajihmcts@users.noreply.github.com>
2022-12-16 17:35:40 +0000

    RDCC-6048:Altered tables for elinks people api (#524)
    Merging script changes

---
+++ src/functionalTest/resources/db/testmigration/V1_50__elinks_alter_tables.sql
@@ -0,0 +1,16 @@
+--Create sequence for dbjudicialdata.judicial_office_authorisation
+CREATE SEQUENCE dbjudicialdata.judicial_office_auth_id_sequence AS integer START 1 OWNED
+BY dbjudicialdata.judicial_office_authorisation.judicial_office_auth_id;
+
+ALTER TABLE dbjudicialdata.judicial_office_authorisation
+ALTER COLUMN judicial_office_auth_id SET DEFAULT nextval('dbjudicialdata.judicial_office_auth_id_sequence');
+
+--Create sequence for dbjudicialdata.judicial_office_appointment
+CREATE SEQUENCE dbjudicialdata.judicial_office_appointment_id_sequence AS integer START 1 OWNED
+BY dbjudicialdata.judicial_office_appointment.judicial_office_appointment_id;
+
+
+ALTER TABLE dbjudicialdata.judicial_office_appointment
+ALTER COLUMN judicial_office_appointment_id SET DEFAULT nextval('dbjudicialdata.judicial_office_appointment_id_sequence');
+
+
