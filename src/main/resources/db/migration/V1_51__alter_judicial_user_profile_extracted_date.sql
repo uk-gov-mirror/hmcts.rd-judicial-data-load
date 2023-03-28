42ace9e5ea5db48ce14523f2877f2764e6369fc7
shivajihmcts <shivaji.gaikwad@hmcts.net>
2023-03-28 15:08:17 +0100

    RDCC-6612:Changes made to raise test pr

--- src/main/resources/db/migration/V1_51__alter_judicial_user_profile_extracted_date.sql
+++ src/main/resources/db/migration/V1_51__alter_judicial_user_profile_extracted_date.sql
@@ -1,2 +1,3 @@
 ALTER TABLE dbjuddata.judicial_user_profile ALTER COLUMN extracted_date TYPE date USING extracted_date::date;
 ALTER TABLE dbjuddata.judicial_office_appointment ALTER COLUMN extracted_date TYPE date USING extracted_date::date;
+
