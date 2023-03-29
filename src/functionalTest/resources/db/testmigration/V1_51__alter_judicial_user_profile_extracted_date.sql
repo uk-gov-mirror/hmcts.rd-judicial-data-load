2630ed7ada6a1f4e6a761b3fe572f235670c1cf3
shivajihmcts <shivaji.gaikwad@hmcts.net>
2023-03-29 14:45:13 +0100

    RDCC-6626:Changes made to run test cases

--- src/functionalTest/resources/db/testmigration/V1_51__alter_judicial_user_profile_extracted_date.sql
+++ src/functionalTest/resources/db/testmigration/V1_51__alter_judicial_user_profile_extracted_date.sql
@@ -1,6 +1,3 @@
---Integration test case changes
-create schema if not exists dbjuddata;
-
 ALTER TABLE judicial_user_profile ALTER COLUMN extracted_date TYPE date USING extracted_date::date;
 ALTER TABLE judicial_office_appointment ALTER COLUMN extracted_date TYPE date USING extracted_date::date;
 
