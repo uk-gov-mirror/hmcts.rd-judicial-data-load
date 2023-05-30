8dbb48b81fa45db9cfbedf8d1e0b8ec75f86ee82
fiyin-cgi <126156952+fiyin-cgi@users.noreply.github.com>
2023-05-30 16:26:10 +0100

    DTSRD-435: Add Base_Location mapping for <service_code> 'BHA1' (#586)

---
+++ src/main/resources/db/migration/V1_59__elinks_insert_judicial_location_mapping_table.sql
@@ -0,0 +1,4 @@
+INSERT INTO dbjudicialdata.judicial_location_mapping (epimms_id,judicial_base_location_id,building_location_name,base_location_name,service_code) VALUES
+	 ('', '1036', '', 'Employment Tribunal England and Wales', 'BHA1'),
+	 ('', '1037', '', 'Employment Tribunal Scotland', 'BHA1');
+COMMIT; 
