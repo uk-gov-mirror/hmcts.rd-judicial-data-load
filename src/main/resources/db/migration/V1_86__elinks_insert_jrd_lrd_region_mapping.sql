068a32aac507f24906fce2b6e9537a881ecba5de
prudhvi-maddineni <111352154+prudhvi-maddineni@users.noreply.github.com>
2023-10-26 11:10:52 +0100

    eLinks : Insert new column in judicial_additional_role table (#618)
    * flyway changes

---
+++ src/main/resources/db/migration/V1_86__elinks_insert_jrd_lrd_region_mapping.sql
@@ -0,0 +1,4 @@
+INSERT INTO dbjudicialdata.jrd_lrd_region_mapping (jrd_region_id,jrd_region,region_id,region) VALUES
+    ('1565',	'Hertfordshire',          '5',	'South East'),
+    ('1787',	'Belfast',                '10',	'Northern Ireland'),
+    ('1533',	'Bath',                    '6',	'South West');
