a7548fd70ff06243fa7f4278e84b95c8bfe2a3e1
sahitya-desireddy <93707379+sahitya-desireddy@users.noreply.github.com>
2023-11-23 14:03:17 +0000

    changing fly way version to 88 and functional fly way (#631)
    * changing fly way version to 88 and functional fly way
    
    * changing fly way version to 88 and functional fly way

---
+++ src/main/resources/db/migration/V1_88__elinks_insert_location_mapping_jrd_lrd.sql
@@ -0,0 +1,23 @@
+INSERT INTO dbjudicialdata.jrd_lrd_region_mapping (jrd_region_id,jrd_region,region_id,region) VALUES
+    ('1819',	'Glasgow (Scotland & NI)',          '11',	'Scotland'),
+    ('2051','Hampshire','5','South East'),
+    ('1268','Oxfordshire','5','South East'),
+    ('2113','St Ives','6','South West'),
+    ('1282','Berkshire','5','South East'),
+    ('1261','Suffolk','5','South East'),
+    ('1967','Chepstow','7','Wales'),
+    ('2033','Edinburgh','11','Scotland'),
+    ('1214','Somerset','6','South West'),
+    ('1973','Yorkshire & Humberside','3','North East'),
+    ('1524','Shropshire','2','Midlands'),
+    ('1630','South Wales','7','Wales'),
+    ('2086','Northumberland','3','North East'),
+    ('1672','Bedfordshire','5','South East');
+
+
+    INSERT INTO dbjudicialdata.judicial_location_mapping (epimms_id, judicial_base_location_id, service_code) VALUES
+        ('498443',	'941',	''),
+        ('701411',	'2218',	''),
+        ('735217',	'2293',	''),
+        ('782795',	'2294',	'');
+    COMMIT;
