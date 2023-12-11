2a496acebb3d689bb92f3e693760a2a41ac52f4a
arshinsalim <97666178+arshinsalim@users.noreply.github.com>
2023-12-11 13:08:05 +0000

    alter jrd_lrd_region (#637)

---
+++ src/functionalTest/resources/db/testmigration/V1_93__elinks_alter_judicIal_userProfile.sql
@@ -0,0 +1,3 @@
+INSERT INTO dbjudicialdata.jrd_lrd_region_mapping (jrd_region_id,jrd_region,region_id,region) VALUES
+    ('1623',	'Essex',          '5',	'South East'),
+    ('2295',	'Field House - Upper Tribunal',                '1',	'London');
